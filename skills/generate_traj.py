e4d3a27dbfa79e8a21623c729500456281835524
Paul Zhou <paul_zhou@brown.edu>
2022-01-28 23:32:01 -0500

    add generate_traj.py

---
+++ skills/generate_traj.py
@@ -0,0 +1,106 @@
+import os
+import pickle
+import argparse
+
+import seeding
+import numpy as np
+
+from skills import utils
+from skills.option_utils import SingleOptionTrial
+
+
+class GenerateTrajectory(SingleOptionTrial):
+    """
+    use the class to generate trajectories using manually-inputted actions, and
+    save the trajectory to a file
+    """
+    def __init__(self):
+        super().__init__()
+        args = self.parse_args()
+        self.params = self.load_hyperparams(args)
+        self.setup()
+
+    def parse_args(self):
+        """
+        parse the inputted argument
+        """
+        parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            parents=[self.get_common_arg_parser()]
+        )
+        parser.add_argument("--file_name", type=str,
+                            help="name of the output file")
+        args = self.parse_common_args(parser)
+        return args
+    
+    def setup(self):
+        # override params
+        self.params['experiment_name'] = 'trajectories'
+        self.params['suppress_action_prunning'] = True
+
+        # setting random seeds
+        seeding.seed(self.params['seed'], np)
+
+        # saving
+        self.saving_dir = os.path.join(self.params['results_dir'], self.params['experiment_name'])
+        self.saving_file = os.path.join(self.saving_dir, self.params['file_name'])
+        utils.create_log_dir(self.saving_dir)
+
+        # make env
+        self.env = self.make_env(self.params['environment'], self.params['seed'])
+    
+    def generate_traj(self):
+        """
+        generate trajectories by human control
+        """
+        print([(i, meaning) for i, meaning in enumerate(self.env.unwrapped.get_action_meanings())])
+        meaning_to_action = {meaning.lower(): i for i, meaning in enumerate(self.env.unwrapped.get_action_meanings())}
+        state = self.env.reset()
+        trajectories = []
+        trajectory = []
+        try:
+            while True:
+                # render env
+                self.env.unwrapped.render()
+
+                # user input an action to take
+                action_input = input()
+                if action_input.isnumeric():
+                    action = int(action_input)
+                else:
+                    action = meaning_to_action[action_input.lower()]
+                    print(action)
+
+                # record the state and ram
+                ram = self.env.unwrapped.ale.getRAM()
+                trajectory.append((np.array(state), ram))
+
+                # take the action
+                next_state, r, done, info = self.env.step(action)
+                state = next_state
+                if done:
+                    state = self.env.reset()
+                    trajectories.append(trajectory)
+                    print(f"finished a trajectory, length: {len(trajectory)}")
+                    trajectory = []
+                    self.save_traj(trajectories)
+        except:
+            # save on error
+            trajectories.append(trajectory)
+            self.save_traj(trajectories)
+    
+    def save_traj(self, trajectories):
+        """
+        save a list of trajectories
+        """
+        with open(self.saving_file, 'wb') as f:
+            pickle.dump(trajectories, f)
+
+
+def main():
+    game = GenerateTrajectory()
+    game.generate_traj()
+
+
+if __name__ == "__main__":
+    main()
