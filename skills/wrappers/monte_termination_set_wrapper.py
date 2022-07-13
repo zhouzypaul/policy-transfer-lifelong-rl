a895f965ae2f2ffac8155f4e3e1771762dbfa68d
Paul Zhou <paul_zhou@brown.edu>
2022-07-13 17:51:56 -0400

    enable initiation classifier with a wrapper

--- skills/wrappers/monte_termination_set_wrapper.py
+++ skills/wrappers/monte_termination_set_wrapper.py
@@ -1,10 +1,8 @@
-import torch
 import numpy as np
 from gym import Wrapper
-from skimage import color
 
 from skills.classifiers.portable_set import EnsembleClassifier
-from skills.wrappers.agent_wrapper import crop_agent_space
+from skills.wrappers.agent_wrapper import build_agent_space_image_stack
 
 
 class MonteTerminationSetWrapper(Wrapper):
@@ -32,16 +30,7 @@ class MonteTerminationSetWrapper(Wrapper):
         next_state, reward, done, info = self.env.step(action)
         # get the agent space input here
         # build the frame stack
-        agent_space_next_state = np.zeros((1, 4, 56, 40))
-        for i, frame in enumerate(self.env.unwrapped.original_stacked_frames):
-            player_pos = self.env.unwrapped.stacked_agent_position[i]
-            obs = crop_agent_space(frame, player_pos)
-            assert obs.shape == (56, 40, 3)
-            obs = color.rgb2gray(obs)  # also concerts to floats
-            obs = np.expand_dims(obs, axis=0)  # add channel dimension, (1, 56, 40)
-            agent_space_next_state[:, i, :, :] = obs
-        tensor_next_state = torch.from_numpy(np.array(agent_space_next_state)).float()
-        assert tensor_next_state.shape == (1, 4, 56, 40), tensor_next_state.shape  # make sure it's agent space observation
+        tensor_next_state = build_agent_space_image_stack(self.env)
         votes, vote_confs = self.clf.get_votes(tensor_next_state)
         # aggregate the votes, vote yes if one of them is yes
         done = np.sum(votes) > 0  # votes are all either 0 or 1
