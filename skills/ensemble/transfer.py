364026049f701042c01b1c18016652c8c9a4fc1f
Paul Zhou <paul_zhou@brown.edu>
2022-07-13 23:59:53 -0400

    [BUGFIX] rm bad function arg

--- skills/ensemble/transfer.py
+++ skills/ensemble/transfer.py
@@ -156,7 +156,6 @@ class TransferTrial(SingleOptionTrial):
                 eval_freq=self.params['eval_freq'],
                 success_threshold_for_well_trained=self.params['success_threshold_for_well_trained'],
                 success_queue_size=self.params['success_queue_size'],
-                start_before_initiation=self.saved_params['initiation_clf']
             )
             # advance to next target
             trained = target
