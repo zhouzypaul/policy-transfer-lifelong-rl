6845fbac2c97e635e31768401a1b22518230ec3a
Paul Zhou <paul_zhou@brown.edu>
2022-09-23 16:08:57 -0400

    plot attention masks with original image

--- skills/plot.py
+++ skills/plot.py
@@ -16,6 +16,24 @@ def parse_args():
 	return args
 
 
+def plot_experiment_data(experiment_name=None, log_file_name='log_file_0.pkl', results_dir='results'):
+	"""
+	the single argument is designed solely for the purpose of calling this function
+	is __main__.py
+	"""
+	if experiment_name is None:
+		# this is used when main() in ran directly from command line
+		args = parse_args()
+		experiment_name = args.experiment_name
+		results_dir = args.results_dir
+
+	experiment_dir = os.path.join(results_dir, experiment_name, log_file_name)
+	plot_learning_curve(experiment_dir)
+
+	img_save_path = os.path.join(results_dir, experiment_name, "learning_curve.png")
+	plt.savefig(img_save_path)
+
+
 def plot_learning_curve(file_path):
 	# open logging file
 	with open(file_path, 'rb') as f:
@@ -45,6 +63,30 @@ def count(f):
     return wrapped
 
 
+def plot_attention_mask_with_original_obs(obs_path, attention_mask_path, save_dir='results'):
+	"""
+	plot the 3 attentions of ensemble-3, along with a original observation
+	from the game screen
+	"""
+	attention_mask = np.load(attention_mask_path)
+	assert len(attention_mask) == 3
+	# plot original obs
+	original_obs = np.load(obs_path)
+	plt.subplot(2, 2, 1)
+	plt.imshow(original_obs)
+	plt.title('Game screen')
+	# plot the attention masks
+	for i in range(3):
+		plt.subplot(2, 2, i+2)
+		plt.imshow(attention_mask[i])
+		plt.title(f"Attention mask {i}")
+		# plt.colorbar()
+	# save fig
+	path = os.path.join(save_dir, 'attention_mask.png')
+	plt.tight_layout()
+	plt.savefig(path)
+
+
 @count
 def plot_attention_diversity(embedding, num_attentions=8, save_dir=None, plot_freq=1):
 	"""
@@ -69,23 +111,16 @@ def plot_attention_diversity(embedding, num_attentions=8, save_dir=None, plot_fr
 	plt.close()
 
 
-def main(experiment_name=None, log_file_name='log_file_0.pkl', results_dir='results'):
-	"""
-	the single argument is designed solely for the purpose of calling this function
-	is __main__.py
-	"""
-	if experiment_name is None:
-		# this is used when main() in ran directly from command line
-		args = parse_args()
-		experiment_name = args.experiment_name
-		results_dir = args.results_dir
-
-	experiment_dir = os.path.join(results_dir, experiment_name, log_file_name)
-	plot_learning_curve(experiment_dir)
-
-	img_save_path = os.path.join(results_dir, experiment_name, "learning_curve.png")
-	plt.savefig(img_save_path)
-
-
 if __name__ == "__main__":
-	main()
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('--attention', '-a', action='store_true', default=True,
+						help='plot attention mask')
+	parser.add_argument('--load', '-l', type=str,
+						help="path to the saved data")
+	args = parser.parse_args()
+
+	if args.attention:
+		plot_attention_mask_with_original_obs(
+			obs_path='results/obs.npy',
+			attention_mask_path='results/coinrun/ensemble-3/2/plots/attention_diversity_393_data.npy',
+		)
